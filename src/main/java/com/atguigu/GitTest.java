06daeb9d57cd00bdc98dee9d38e1c464bd726f53
rlk <1195733485@qq.com>
2022-06-02 09:56:40 +0800

    新建

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * @author: rlk
+ * @date: 2022/6/2
+ * Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("新建");
+    }
+}
